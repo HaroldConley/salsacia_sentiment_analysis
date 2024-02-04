dc9e9f2b421d214814f709eecee2322f76bbac8b
Harold Conley <106288869+HaroldConley@users.noreply.github.com>
2024-02-04 15:43:19 -0300

    Update app.py
    Se agrega función que genera comentario

--- app.py
+++ app.py
@@ -23,6 +23,26 @@ def sentiment_analysis(api_key, comentario):
     return response.choices[0].message.content
 
 
+def comentario_generator(api_key, comentario_base):
+    # Inicializando el cliente de OpenAI
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        temperature=1,
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": """
+            Te voy a entregar un comentario sobre un candidato.
+            Necesito que generes un nuevo comentario corto, en lenguaje popular, apoyando el comentario entregado.
+            Entrega el resultado directo, sin textos adicionales.
+            """},
+            {"role": "user", "content": f"""Comentario: {comentario_base}"""}
+        ]
+    )
+
+    return response.choices[0].message.content
+
+
 # Título WebApp
 st.title('Analizador de Sentimientos')
 
@@ -38,7 +58,13 @@ if st.button('Analizar'):
     with st.spinner('Analizando tu comentario...'):
         # Procesamiento
         api_key = os.environ['OPENAI_API_KEY']
-        response = sentiment_analysis(api_key, comentario)
 
-        # Muestra de la respuesta
-        st.markdown(response, unsafe_allow_html=True)
+        tendencia = sentiment_analysis(api_key, comentario)
+
+        # Muestra el resultado del análisis
+        st.markdown(tendencia, unsafe_allow_html=True)
+
+        # Genera un nuevo comentario basado en el ingresado
+        new_comentario = comentario_generator(api_key, comentario)
+        st.markdown(new_comentario, unsafe_allow_html=True)
+
